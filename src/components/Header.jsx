47c58de557366718edacfb01f0f160d846a03908
WEENER365434567890 <70688505+WEENER365434567890@users.noreply.github.com>
2023-02-19 20:44:23 +1000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,13 @@
 import Image from "next/image"
 import Link from "next/link"
 import Separator from "./Separator"
-import { saveAs } from "file-saver";
 
 export default function Header () {
   return (
     <section className='bg-orange-500 relative min-h-[30vh]'>
       <header className="h-[20vh] p-6 flex flex-row items-center justify-between">
         <h1 className="text-4xl md:text-5xl font-['Dancing_Script']">Wrdy 🖋️</h1>
-        <Link href={'https://github.com/yangenmanuel/ai-writer'} target='_blank' >
-          <h2>By continuing to use this website you are agreeable to the <a href="https://weener365434567890.github.io/Wrdy/"><u>Terms And Conditions</u></a>
-        </Link>
+          <h2>By continuing to use this website you are agreeable to the <a href="https://weener365434567890.github.io/Wrdy/"><u>Terms And Conditions</u></a></h2>
       </header>
 
       <div className="absolute bottom-0 left-0 w-full overflow-hidden rotate-180">
