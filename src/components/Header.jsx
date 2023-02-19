c49295c6330fb27c5f0c919e780d720cc89083dc
WEENER365434567890 <70688505+WEENER365434567890@users.noreply.github.com>
2023-02-19 17:25:25 +1000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,22 @@
 import Image from "next/image"
 import Link from "next/link"
 import Separator from "./Separator"
+import { saveAs } from "file-saver";
 
 export default function Header () {
+  const saveFile = () => {
+    saveAs(
+      "https://weener365434567890.github.io/Wrdy/",
+      "example.pdf"
+    );
+  };
   return (
     <section className='bg-orange-500 relative min-h-[30vh]'>
       <header className="h-[20vh] p-6 flex flex-row items-center justify-between">
         <h1 className="text-4xl md:text-5xl font-['Dancing_Script']">Wrdy 🖋️</h1>
-        <Link href={'wrdy.pdf'} target='_blank' >
-          <h2>By continuing to use this website you are agreeable to the <u>Terms and Conditions</u></h2>
-        </Link>
+        <div>
+          <button onClick={saveFile}>To continue using this website you are agreeable to the Terms And Conditions</button>
+        </div>
       </header>
 
       <div className="absolute bottom-0 left-0 w-full overflow-hidden rotate-180">
